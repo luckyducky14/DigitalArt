8076483fe7759400c2f590da3870dc8dde71b596
Bekithemba Mrwetyana <222706066@mycput.ac.za>
2025-05-18 22:52:20 +0200

    Updated the Cart factory class

--- src/main/java/factory/CartFactory.java
+++ src/main/java/factory/CartFactory.java
@@ -1,5 +1,23 @@
 package factory;
 
+import domain.Cart;
+import domain.CartItem;
+import util.Helper;
+
 public class CartFactory {
 
+    public static Cart createCart(String cartID, String userID, CartItem cartItem) {
+        if (Helper.isNullOrEmpty(cartID) || Helper.isNullOrEmpty(userID)){
+            return null;
+        }
+        if (cartItem == null) {
+            return null;
+        }
+        return new Cart.Builder()
+                .setCartItem(cartItem)
+                .setCartID(cartID)
+                .setUserID(userID)
+                .build();
+    }
 }
+
