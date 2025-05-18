998f921efea8f155c492f55aed60334a443e458c
Bekithemba Mrwetyana <222706066@mycput.ac.za>
2025-05-18 22:48:51 +0200

    Updated the Payment Factory class

--- src/main/java/factory/PaymentFactory.java
+++ src/main/java/factory/PaymentFactory.java
@@ -1,25 +1,42 @@
 package factory;
 
 import domain.Payment;
+import util.Helper;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public class PaymentFactory {
 
-    public static Payment createPayment(int paymentID, LocalDateTime paymentDate, double amount, String status, String method){
+    public static Payment createPayment(String paymentID, LocalDate paymentDate, double amount, String status, String method){
 
-        if (amount <= 0 || status == null || method == null){
+        if(amount <= 0 || Helper.isNullOrEmpty(status) || Helper.isNullOrEmpty(method)){
             return null;
         }
-
+        
         return new Payment.Builder()
                 .setPaymentID(paymentID)
-                .setPaymentDate(paymentDate)
                 .setAmount(amount)
                 .setStatus(status)
                 .setMethod(method)
-
                 .build();
     }
+    public static Payment createPayment(LocalDate paymentDate, double amount, String status, String method){
+        if(amount <= 0 || Helper.isNullOrEmpty(status) || Helper.isNullOrEmpty(method)){
+            return null;
+        }
+        if(paymentDate == null){
+            return null;
+        }
+
+        LocalDate now = LocalDate.now();
+        return new Payment.Builder()
+                .setPaymentDate(now)
+                .setAmount(amount)
+                .setStatus(status)
+                .setMethod(method)
+                .build();
+
+    }
+
 
 }
