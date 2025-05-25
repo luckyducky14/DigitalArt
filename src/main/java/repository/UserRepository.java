48a619a8bc0d67b9aaeec06ca863aa536d4f0f62
pc <221376909@mycput.ac.za>
2025-05-25 17:53:58 +0200

    commit and push UserRepository.java

---
+++ src/main/java/repository/UserRepository.java
@@ -0,0 +1,15 @@
+
+package repository;
+
+import domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    // Common query methods for User entity
+    User findByEmail(String email);
+    User findByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+}
