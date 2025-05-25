7e13cf025d96793cb3b9dba66377824f295ab3ce
Bekithemba Mrwetyana <222706066@mycput.ac.za>
2025-05-25 20:18:23 +0200

    Updated CartRepository

--- src/main/java/repository/CartRepository.java
+++ src/main/java/repository/CartRepository.java
@@ -1,9 +1,18 @@
 package repository;
 
 import domain.Cart;
+import domain.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, String> {
+
+    List<Cart> findByUserID(String userID);
+    List<Cart> findByCartID(String cartID);
+    List<Cart> findByCartItem(CartItem cartItem);
+
+
 }
