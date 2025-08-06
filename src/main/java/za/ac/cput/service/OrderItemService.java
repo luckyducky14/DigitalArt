0a77fbd0df0eedd36b9a62c294c39d7248677b53
Thimna <222213973@mycput.ac.za>
2025-08-06 18:18:35 +0200

    Added classes

---
+++ src/main/java/za/ac/cput/service/OrderItemService.java
@@ -0,0 +1,48 @@
+package za.ac.cput.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.OrderItem;
+import za.ac.cput.repository.OrderItemRepository;
+
+import java.util.List;
+
+@Service
+public class OrderItemService  {
+
+    private final OrderItemRepository repository;
+
+    @Autowired
+    public OrderItemService(OrderItemRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public OrderItem create(OrderItem orderItem) {
+        return repository.save(orderItem);
+    }
+
+    @Override
+    public OrderItem read(Integer itemID) {
+        return repository.findById(itemID).orElse(null);
+    }
+
+    @Override
+    public OrderItem update(OrderItem orderItem) {
+        return repository.save(orderItem);
+    }
+
+    @Override
+    public boolean delete(Integer itemID) {
+        if (repository.existsById(itemID)) {
+            repository.deleteById(itemID);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<OrderItem> getAll() {
+        return repository.findAll();
+    }
+}
