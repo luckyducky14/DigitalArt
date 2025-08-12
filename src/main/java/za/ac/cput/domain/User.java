3f54581f7a9e1659c6a9919d4a043bc2a6140d28
luckyducky14 <221376909@mycput.ac.za>
2025-08-12 22:22:46 +0200

    Commit and push User.java and UserFactory.java

--- src/main/java/za/ac/cput/domain/User.java
+++ src/main/java/za/ac/cput/domain/User.java
@@ -3,75 +3,50 @@ package za.ac.cput.domain;
 /*
 User.java
 User POJO class
-Author: Luvoo Nana 221376909
+Author: Luvo Nana 221376909
 Date: 08 May 2025
 */
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import javax.management.relation.Role;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
 public class User {
 
     @Id
-    private int userId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId; //long
     private String lastName;
     private String firstName;
     private String email;
     private String password;
+    private LocalDateTime lastLogin;
+    private LocalDate createDate;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
-    public int getUserId() {
-        return userId;
+
+    // Protected default constructor for JPA
+    protected User() {}
+
+    private User(Builder builder) {
+        this.userId = builder.userId;
+        this.lastName = builder.lastName;
+        this.firstName = builder.firstName;
+        this.email = builder.email;
+        this.password = builder.password;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public User() {
-    }
-
-    public User(int userId, String lastName, String firstName, String email, String password) {
-        this.userId = userId;
-        this.lastName = lastName;
-        this.firstName = firstName;
-        this.email = email;
-        this.password = password;
-    }
-
-    public int getUserId(int userId) {
-        return userId;
-    }
-
-    public String getLastName(String lastName) {
-        return lastName;
-    }
-
-    public String getFirstName(String firstName) {
-        return firstName;
-    }
-
-    public String getEmail(String email) {
-        return email;
-    }
-
-    public String getPassword(String password) {
-        return password;
-    }
+    // Getters
+    public int getUserId() { return userId; }
+    public String getLastName() { return lastName; }
+    public String getFirstName() { return firstName; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
 
     @Override
     public String toString() {
@@ -84,17 +59,9 @@ public class User {
                 '}';
     }
 
-    private User(Builder builder) {
-        this.userId = builder.userId;
-        this.lastName = builder.lastName;
-        this.firstName = builder.firstName;
-        this.email = builder.email;
-        this.password = builder.password;
-    }
-
+    // Builder pattern
     public static class Builder {
-
-        private int userId;
+        private int userId; //long
         private String lastName;
         private String firstName;
         private String email;
@@ -104,36 +71,30 @@ public class User {
             this.userId = userId;
             return this;
         }
-
         public Builder setLastName(String lastName) {
             this.lastName = lastName;
             return this;
         }
-
         public Builder setFirstName(String firstName) {
             this.firstName = firstName;
             return this;
         }
-
         public Builder setEmail(String email) {
             this.email = email;
             return this;
         }
-
         public Builder setPassword(String password) {
             this.password = password;
             return this;
         }
-
-        public Builder copy(User item) {
-            this.userId = item.userId;
-            this.lastName = item.lastName;
-            this.firstName = item.firstName;
-            this.email = item.email;
-            this.password = item.password;
+        public Builder copy(User user) {
+            this.userId = user.userId;
+            this.lastName = user.lastName;
+            this.firstName = user.firstName;
+            this.email = user.email; //Remove
+            this.password = user.password;
             return this;
         }
-
         public User build() {
             return new User(this);
         }
--- src/main/java/za/ac/cput/factory/UserFactory.java
+++ src/main/java/za/ac/cput/factory/UserFactory.java
@@ -1,15 +1,20 @@
 package za.ac.cput.factory;
 
 import za.ac.cput.domain.User;
+import za.ac.cput.util.Helper;
 
 public class UserFactory {
 
-    public static User createUser(int userId, String lastName, String firstName, String email, String password) {
+    public static User createUser(String lastName, String firstName, String email, String password) {
 
-        return new User.Builder().setUserId(userId)
+        if (Helper.isNullOrEmpty(firstName) || Helper.isNullOrEmpty(lastName) || Helper.isNullOrEmpty(password) || Helper.isNullOrEmpty(email)) {
+            return null;
+        }
+        // Contact contact = ContactFactory.createContact(email,
+        return new User.Builder()
                 .setLastName(lastName)
                 .setFirstName(firstName)
-                .setEmail(email)
+                .setEmail(email) //Remove
                 .setPassword(password)
                 .build();
     }
