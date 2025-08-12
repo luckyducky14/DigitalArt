08571e09523e4b9ed1b1bad9aeaa1cbd938448a1
Bekithemba Mrwetyana <222706066@mycput.ac.za>
2025-08-12 18:44:08 +0200

    Database configeration

--- src/main/java/za/ac/cput/repository/CategoryRepository.java
+++ src/main/java/za/ac/cput/repository/CategoryRepository.java
@@ -7,12 +7,13 @@ Author: Abethu Ngxitho 221297820
 Date: 24 May 2025
 */
 
+import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-
-public interface CategoryRepository extends JpaRepository {
+@Repository
+public interface CategoryRepository extends JpaRepository <Category, String>{
 
     List<Category> findbyName(String name);
 
